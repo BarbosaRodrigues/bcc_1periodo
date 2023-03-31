183aeec313fdf7be057d34ab3d7863e93646d770
BarbosaRodrigues <taisbarbosarodrigues2004@gmail.com>
2023-03-30 23:24:30 -0300

    py submissao 1

---
+++ intro_prog/pratica01/prog01.py
@@ -0,0 +1,19 @@
+print("Linha 1\nLinha 2\n\nLinha 3\n\n\nLinha 4")
+
+print("\n_________________________\n")
+
+print('''
+Linha 1
+Linha 2
+Linha 3
+Linha 4
+
+''')
+
+print("\n_________________________\n")
+
+print(""" + - - - - - - - +\n
+ |               |\n
+ |   the box     |\n
+ |               |\n
+ + - - - - - - - +\n\n""")
---
+++ intro_prog/pratica01/prog02.py
@@ -0,0 +1,3 @@
+#print("teste conhecendo mensagens de erro\n\n")
+
+print("Inteiro:",5,"\nFloat:",5.987,"\nBooleano:",True)
